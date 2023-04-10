e6efa676eac67b0d02395151fe059c3a12cdba22
NCCoder1 <119767145+NCCoder1@users.noreply.github.com>
2023-04-10 04:13:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload=function()
+{
+var el=document.getElementById('button');
+    el.onclick=function(){
+        var my_text = document.getElementById("pass");
+        if(my_text.value == "idontdomyhomework") { // Replace "" with password inside of it
+          var iframe = document.getElementById("iframe-elm");
+          iframe.src = "https://ultramath2.callixo.xyz/uv/service/hvtrs8%2F-neg%2C7%3B8qc%2Ccmm-"; // Replace "" with the place you'd like iframe to go to inside
+        } else {
+          alert("Wrong Password");
+          }
+    }
+
+}
